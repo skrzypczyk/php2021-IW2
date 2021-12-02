602809a8f205a693579678818bed41e21c48a182
Yves skrzypczyk <y.skrzypczyk@gmail.com>
2021-12-02 11:18:53 +0100

    connexion bdd

--- www/Core/BaseSQL.class.php
+++ www/Core/BaseSQL.class.php
@@ -2,10 +2,21 @@
 
 namespace App\Core;
 
+
 class BaseSQL
 {
+    private $pdo;
+
     public function __construct()
     {
+        //Faudra intégrer le singleton
+        try{
+            //Connexion à la base de données
+            $this->pdo = new \PDO( "mysql:host=database;dbname=mvcdocker2" ,"root" , "password" );
+        }catch(\Exception $e){
+            die("Erreur SQL".$e->getMessage());
+        }
+
     }
 
     protected function save()
